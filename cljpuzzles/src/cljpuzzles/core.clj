4eb2e5b38e1eea37c50fb093baa336bcacec06a0
Jacob Poulsgaard Tjørnholm <chopmo@gmail.com>
2016-01-16 16:23:56 +0100

    Added clj part with day 5

---
+++ cljpuzzles/src/cljpuzzles/core.clj
@@ -0,0 +1,24 @@
+(ns cljpuzzles.core
+  (:require [clojure.string :as s]))
+
+(defn day5-1
+  []
+  (let [strings (s/split-lines (slurp "/Users/jtj/c/advent/cljpuzzles/day5.txt"))
+        nice? (fn [s]
+                (and
+                 (>= (count (re-seq #"[aeiou]" s)) 3)
+                 (re-seq #"(.)\1" s)
+                 (not (re-seq #"ab|cd|pq|xy" s))))]
+    (count (filter nice? strings))))
+
+(defn day5-2
+  []
+  (let [strings (s/split-lines (slurp "/Users/jtj/c/advent/cljpuzzles/day5.txt"))
+        nice? (fn [s]
+                (and
+                 (re-seq #"(..).*\1" s)
+                 (re-seq #"(.).\1" s)))]
+    (count (filter nice? strings))))
+
+
+(day5-2)
